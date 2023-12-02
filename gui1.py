68ddc1a342380c7a0fcea1dde1f57280a3aa2813
JEFRZONE <jefrinjohnj3@gmail.com>
2023-12-02 20:33:53 +0530

    2 gui programs

--- gui1.py
+++ gui1.py
@@ -8,29 +8,51 @@ def printf():
     passw = password.get()
     if user in d1 and (d1[user]==passw):
         print("sucess")
-        #gui2()
+        root.destroy()
+        gui2()
     else:
         clearall()
         canvas1.create_text( 100, 330, text = "wrong login credentials",font=("Franklin Gothic Medium", "12"),anchor="n")
 
     # else the login credientials are wrong try and retry
-'''
+
 def gui2():
-    canvas2 = Canvas( root, width = 900, 
-				height = 900 ) 
-
+   
+    root2=Tk()
+    root2.geometry("600x600")
+    bg1 = PhotoImage(file = "white.png")
+    canvas2 = Canvas( root2, width = 900, 
+                    height = 900 ) 
     canvas2.pack(fill = "both", expand = True) 
-
-    # Display image 
-    
-    canvas1.create_image( 0, 0, image = bg, 
-                        anchor = "nw") 
-
-    # Add Text 
+    canvas2.create_image( 0, 0, image = bg1, 
+                        anchor = "nw")
     canvas2.create_text( 50, 10, text = "Welcome",fill="maroon",font=("Franklin Gothic Medium", "12"),anchor="n")
-    canvas2.create_text( 50, 70, text=" USERID:   ",fill="maroon",font=("Franklin Gothic Medium", "10"),anchor="n") 
+    profile=root2.Label( text=" PROFILE  ", width=50, height=10 ,master=root2  )
+    ''',fill="maroon",font=("Franklin Gothic Medium", "10"),anchor="n"'''
+    profile.pack()
+    button1_canvas2 = canvas2.create_window(  100,70,
+									anchor = "nw",
+                                    height=30,
+                                    width=100, 
+									window = profile) 
     
-'''
+    '''label=root2.Label( 50, 70, text=" PROFILE  ",fill="maroon",font=("Franklin Gothic Medium", "10"),anchor="n") 
+    label2=canvas2.create_text( 50, 130, text="APPLICATION",fill="maroon",font=("Franklin Gothic Medium", "10"),anchor="n")
+    label.pack()
+    label2.pack()
+    label.bind("<Buttton-1>",gui3)'''
+
+
+def gui3():
+    root3=Tk()
+    root3.geometry("600x600")
+    bg2 = PhotoImage(file = "white.png")
+    canvas3 = Canvas( root3, width = 900, 
+                    height = 900 ) 
+    canvas3.pack(fill = "both", expand = True) 
+    canvas3.create_image( 0, 0, image = bg2, 
+                        anchor = "nw")
+    canvas3.create_text( 50, 10, text = "Welcome",fill="maroon",font=("Franklin Gothic Medium", "12"),anchor="n")
 
 def clearall():
 
@@ -96,67 +118,6 @@ button3_canvas = canvas1.create_window( 100, 190, anchor = "nw",
                                         width=50,  
 									window = button3) 
 
-root.mainloop() 
-
-print(username)
-
-# Import module 
-from tkinter import *
-def printf():
-    print(username.get())
-    print(password.get())
-
-# Create object 
-root = Tk() 
-
-# Adjust size 
-root.geometry("400x400") 
-
-# Add image file 
-bg = PhotoImage(file = "rset.png")
-
-# Create Canvas 
-canvas1 = Canvas( root, width = 900, 
-				height = 900 ) 
-
-canvas1.pack(fill = "both", expand = True) 
-
-# Display image 
-canvas1.create_image( 0, 0, image = bg, 
-					anchor = "nw") 
-
-# Add Text 
-canvas1.create_text( 200, 230, text = "Welcome",font=("Franklin Gothic Medium", "12"),anchor="n")
-canvas1.create_text( 100, 10, text=" USERID:   ",fill="black",font=("Franklin Gothic Medium", "10"),anchor="n") 
-canvas1.create_text( 100, 70, text="PASSWORD: ",fill="black",font=("Franklin Gothic Medium", "10"),anchor="n") 
-
-# Create Buttons 
-#button1 = Button( root, text = "Exit") 
-button3 = Button( root, text = "LOGIN",fg="white",bg="black",command=printf)
-#create entry for accepting the inputs from the user
-username=Entry(root,width=100,fg="white",bg="black")
-password=Entry(root,width=100,fg="white",bg="black")
-#button2 = Button( root, text = "Reset") 
-
-# Display Buttons 
-button1_canvas = canvas1.create_window(  150,10,
-									anchor = "nw",
-                                    height=30,
-                                    width=100, 
-									window = username) 
-
-button2_canvas = canvas1.create_window( 150, 70, 
-									anchor = "nw",
-                                    height=30,
-                                    width=100,
-									window = password) 
-
-button3_canvas = canvas1.create_window( 150, 130, anchor = "nw",
-                                        height=50,
-                                        width=50,  
-									window = button3) 
-
-# Execute tkinter 
 root.mainloop() 
 
 print(username)
