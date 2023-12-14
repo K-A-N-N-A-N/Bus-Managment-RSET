b0d6372018de4bb7d396d4fa67e0d209ffbf6bbd
Jus7in4ji <justinajikoikamolath@gmail.com>
2023-12-14 17:21:20 +0530

    BusID Database connection established

--- Bus_ID.py
+++ Bus_ID.py
@@ -1,10 +1,23 @@
 from customtkinter import *
 from PIL import Image
 
+import mysql.connector
+conn = mysql.connector.connect(host='localhost',username='root',password='456ASDcvb###',database='bustrial')
+busdb=conn.cursor()
+
 def V_BusID():
+    bbuid="U2101"
     VID_app = CTk()
     VID_app.geometry("500x400")
     set_appearance_mode("white")
+    
+    busdb.execute('select bstudents.UID,bstudents.BusID,bstudents.BusNo,busdet.Route from bstudents join busdet on bstudents.Busno=busdet.Busno')
+    for row in busdb:
+         if row[0]==bbuid:
+              busid=row[1]
+              busno=row[2]
+              busroute=row[3]
+              
 
     #frame = CTkFrame(master=app, fg_color="red", border_color="blue", border_width=2)
     #frame.pack(expand = True)
@@ -22,17 +35,25 @@ def V_BusID():
     #barcode_label = CTkLabel(VID_app,image = barcode_image, text="")
     #barcode_label.grid(row = 0, column = 0)
 
-    Uid_label = CTkLabel(master=VID_app, text= "UID : ",font= ("courier new", 15), text_color = "red" )
+    Uid_label = CTkLabel(master=VID_app, text= "UID : uii",font= ("courier new", 15), text_color = "red" )
     Uid_label.place(relx = 0.2, rely = 0.5, anchor = "e" )
+    Uid_db = CTkLabel(master=VID_app, text= bbuid,font= ("courier new", 15), text_color = "red" )
+    Uid_db.place(relx = 0.5, rely = 0.5, anchor = "w" )
 
     BusId_label = CTkLabel(master=VID_app, text= "Bus ID : ",font= ("courier new", 15), text_color = "red" )
     BusId_label.place(relx = 0.2, rely = 0.6, anchor = "e" )
+    BusId_db = CTkLabel(master=VID_app, text= busid,font= ("courier new", 15), text_color = "red" )
+    BusId_db.place(relx = 0.5, rely = 0.6, anchor = "center" )
 
     BusNo_label = CTkLabel(master=VID_app, text= "Bus No : ",font= ("courier new", 15), text_color = "red" )
     BusNo_label.place(relx = 0.2, rely = 0.7, anchor = "e" )
+    BusNo_db = CTkLabel(master=VID_app, text= busno,font= ("courier new", 15), text_color = "red" )
+    BusNo_db.place(relx = 0.5, rely = 0.7, anchor = "center" )
 
     Route_label = CTkLabel(master=VID_app, text= "Route : ",font= ("courier new", 15), text_color = "red" )
     Route_label.place(relx = 0.2, rely = 0.8, anchor = "e" )
+    BusId_db = CTkLabel(master=VID_app, text= busroute,font= ("courier new", 15), text_color = "red" )
+    BusId_db.place(relx = 0.5, rely = 0.8, anchor = "center" )
 
     VID_app.mainloop()
 
@@ -51,18 +72,33 @@ def Pay():
     Rate_entry.place(relx = 0.5, rely = 0.45, anchor = "center")
 
     def check():
-        space = "Y"
-        if (space == "Y"):
-            print("Space Available")
-        else:
-            print("No Space aavailable")
+        route=Route_entry.get()
+        busdb.execute("select * from busdet")
+        flagr=0 
+        for row in busdb:
+            if route ==row[1]:
+                flagr=1
+                if row[3]>0:
+                    print("Seats are Available")
+                else:
+                    print("Unavailable")
+        if flagr==0:
+                print("given route doesn't exist")            
+        
 
     def pay():
-        space = "Y"
-        if (space == "Y"):
-            print("Payment Successful")
-        else:
-            print("No Seats Available")
+        route=Route_entry.get()
+        busdb.execute("select * from busdet")
+        flagr=0 
+        for row in busdb:
+            if route ==row[1]:
+                flagr=1
+                if row[3]>0:
+                    print("payment succesfull")
+                else:
+                    print("seats Unavailable")
+        if flagr==0:
+                print("given route doesn't exist") 
 
     Check_btn = CTkButton(master=Pay_app, text = "Check", corner_radius=25, fg_color="#4158D0", hover_color="#C850C0",
                           border_color="#FFCC70", border_width= 2, command=check)
