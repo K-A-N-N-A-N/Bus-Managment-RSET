88c08f52bd1231a9b59f526e56af8f460fcdff83
Jus7in4ji <justinajikoikamolath@gmail.com>
2023-12-17 17:54:20 +0530

    Registration Connected to DBMS

--- registration.py
+++ registration.py
@@ -3,6 +3,12 @@ import customtkinter
 from customtkinter import *
 from PIL import Image, ImageTk
 
+import mysql.connector
+conn = mysql.connector.connect(host='localhost',username='root',password='456ASDcvb###',database='RSETbus')
+busdb=conn.cursor()
+UId= "U3344"
+rflag = 0
+busn=0
 def register():
     reg_app = CTk()
     reg_app.title("Register / Pay")
@@ -27,7 +33,41 @@ def register():
     Route_entry.place(x = 50, y = 110)
 
     def check():
-        x = "Y" # for checking with dbms
+        global rflag
+        global busn
+        route=Route_entry.get()
+        flag= 0
+        rflag = 0
+        busdb.execute('select * from bus')
+        Note1 = CTkLabel(master=frame,text="",font=('Century Gothic',20))
+        Note2 = CTkLabel(master=frame,text="",font=('Century Gothic',20))
+        Note3 = CTkLabel(master=frame,text="",font=('Century Gothic',20))
+        for row in busdb:
+            if row[1]==route:
+                flag = 1
+                if row[3]>0:
+                    rate = row[2]
+                    Rate_entry = CTkEntry(master=frame,width=220, placeholder_text=rate)
+                    Rate_entry.place(x = 50, y = 145)
+                    Note2.destroy()
+                    Note3.destroy()
+                    rflag = 1
+                    busn = row[0]
+                    Note1 = CTkLabel(master=frame,text=" Seats Are Available",font=('Century Gothic',20))
+                    Note1.place(x = 50, y = 240)
+                    
+                else:
+                    Note1.destroy()
+                    Note3.destroy()
+                    Note2 = CTkLabel(master=frame,text="Seats Unavailable",font=('Century Gothic',20))
+                    Note2.place(x = 50, y = 240)
+        if flag==0:
+            Note1.destroy()
+            Note2.destroy()
+            Note3 = CTkLabel(master=frame,text="Route Does Not Exist ",font=('Century Gothic',20))
+            Note3.place(x = 50, y = 240)
+
+        '''x = "Y" # for checking with dbms
         y = 1500 # for sending rate to the label
         if x == "Y":
             Rate_entry = CTkEntry(master=frame,width=220, placeholder_text=y)
@@ -41,66 +81,40 @@ def register():
             Note.place(x = 50, y = 240)
             Note = CTkLabel(master=frame,text="Route Doesnt Exist. Enter valid Route",font=('Century Gothic',20))
             Note.place(x = 50, y = 240)
-            return 0
+            return 0'''
 
     def pay():
-        chk = check()
-        if chk == 1:
-            Note = CTkLabel(master=frame,text="Payment Completed",font=('Century Gothic',20))
-            Note.place(x = 50, y = 240)
-    
+        print("deez nits")
+        if rflag == 1:
+            print("deez nuts")
+            busdb.execute('select count(*) from bstudent')
+            for row in busdb:
+                num = row[0]+1
+                BUID= "B0"+str(num)
+            print(BUID)    
+            busdb.execute('select * from student')
+            for row in busdb:
+                if row[0]==UId:
+                    if row[2]==1:
+                        print("paid already")
+                        Note = CTkLabel(master=frame,text="Already paid",font=('Century Gothic',20))
+                        Note.place(x = 50, y = 270)
+                    else:
+                        busdb.execute('insert into bstudent values (%s,%s,%s)',(BUID,UId,busn))
+                        Note = CTkLabel(master=frame,text="Payment Completed",font=('Century Gothic',20))
+                        Note.place(x = 50, y = 270)
+                        busdb.execute('update student set payment_status = 1 where UID = %s',(UId,))    
+                        conn.commit()
+
+
+
     Check_btn = CTkButton(master=frame,width=100, height=20, text = "Check", compound="left", corner_radius=6, fg_color="white", text_color="black", command=check)
     Check_btn.place(x = 50, y = 200)
 
     pay_btn = CTkButton(master=frame,width=100, height=20, text = "Pay", compound="left", corner_radius=6, fg_color="white", text_color="black", command=pay)
     pay_btn.place(x = 170, y = 200)
 
-    ''' 
-    def check():
-        route=Route_entry.get()
-        busdb.execute("select * from busdet")
-        flagr=0 
-        for row in busdb:
-            if route ==row[1]:
-                flagr=1
-                if row[3]>0:
-                    print("Seats are Available")
-                    print("Rate is :" + str(row[2]))
-
-                else:
-                    print("Unavailable")
-        if flagr==0:
-                print("given route doesn't exist")            
-        
-        
-    def pay():
-        route=Route_entry.get()
-        busdb.execute('select count(*)from student')
-        for row in busdb:
-            num = row[0]+1
-            BUID="B0"+str(num)
-        busdb.execute("select * from busdet")
-        flagr=0 
-        for row in busdb:
-            if route ==row[1]:
-                flagr=1
-                if row[3]>0:
-                    print("payment succesfull")
-                    print("Your BusID =" + BUID )    
-                else:
-                    print("seats Unavailable")
-        if flagr==0:
-                print("given route doesn't exist") 
-    '''
-
-    #Check_btn = CTkButton(master=reg_app, text = "Check", corner_radius=25, fg_color="#4158D0", hover_color="#6450c8",
-                          #border_color="#FFCC70", border_width= 2, command=check)
-    #Check_btn.place(relx = 0.4, rely = 0.6, anchor = "e")
-
-    #Check_btn = CTkButton(master=reg_app, text = "Pay", corner_radius=25, fg_color="#4158D0", hover_color="#6450c8",
-                          #border_color="#FFCC70", border_width= 2, command=pay)
-    #Check_btn.place(relx = 0.9, rely = 0.6, anchor = "e")
-
+   
     reg_app.mainloop()
 
-#register()
+register()
